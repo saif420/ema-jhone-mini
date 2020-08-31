d12b502e8e9dd9072b251802d305d251ea79479d
saif420 <saifmahmud42069@gmail.com>
2020-08-30 22:04:53 -0700

    cart add

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,6 +6,11 @@ const Header = () => {
     return (
         <div className="header">
             <img src={logo} alt=""/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Review</a>
+                <a href="mange">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
